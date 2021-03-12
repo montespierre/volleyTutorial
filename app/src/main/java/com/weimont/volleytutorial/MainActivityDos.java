2785fa30616ee27181634bc5c0ffc9e1ab22e3cb
unknown <montespierre@gmail.com>
2021-03-11 21:58:00 -0500

    separando del array

--- app/src/main/java/com/weimont/volleytutorial/MainActivityDos.java
+++ app/src/main/java/com/weimont/volleytutorial/MainActivityDos.java
@@ -9,14 +9,20 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivityDos extends AppCompatActivity {
 
     TextView vTextview;
     RequestQueue requestQueue;
     private static final String URL1 = "https://my-json-server.typicode.com/typicode/demo/comments";
+    private static final String URL2 = "https://my-json-server.typicode.com/typicode/demo/posts";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +34,8 @@ public class MainActivityDos extends AppCompatActivity {
         //UI
         initUI();
 
-        stringRequest();
+        //stringRequest();
+        jsonArrayRequest();
 
 
     }
@@ -59,5 +66,43 @@ public class MainActivityDos extends AppCompatActivity {
 
         );
         requestQueue.add(request);
+    }
+
+    private void jsonArrayRequest(){
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                URL2,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        int size = response.length();
+
+                        vTextview.setText("");
+                        
+                        for(int i = 0; i<size; i++){
+
+                            try {
+                                JSONObject jsonObject = new JSONObject(response.get(i).toString());
+                                String title = jsonObject.getString("title");
+                                vTextview.append("Title: " + title + "\n");
+                            }catch (JSONException e){
+                                e.printStackTrace();
+                            }
+
+
+
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+        requestQueue.add(jsonArrayRequest);
     }
 }
